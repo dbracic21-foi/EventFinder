d68e676e31a7af1e1afddeee1368311d233296d9
dbracic21-foi <dbracic21@student.foi.hr>
2023-11-15 15:40:51 +0100

    Made validation for Login and Registration

--- Software/app/src/main/java/com/example/eventfinder/LoginActivity.kt
+++ Software/app/src/main/java/com/example/eventfinder/LoginActivity.kt
@@ -2,24 +2,52 @@ package com.example.eventfinder
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
-    private var textviewNeedAccount: TextView? = null;
+    private var ButtonLogin : Button? = null
+    private var textviewNeedAccount: TextView? = null
+    private var editTextUserNameOrEmail : EditText? = null
+    private var editTextPassword : EditText? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        ButtonLogin = findViewById<View>(R.id.buttonLogin) as Button
         textviewNeedAccount = findViewById<View>(R.id.textviewNeedAccount) as TextView
+        editTextUserNameOrEmail = findViewById<View>(R.id.editTextUsernameOrEmail) as EditText
+        editTextPassword = findViewById<View>(R.id.editTextPasswordLogin) as EditText
+        ButtonLogin!!.isEnabled = false
+
+        val textWatcher = object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                validateFields()
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        }
+        editTextUserNameOrEmail!!.addTextChangedListener(textWatcher)
+        editTextPassword!!.addTextChangedListener(textWatcher)
+
         textviewNeedAccount!!.setOnClickListener{
             openRegisterForm()
         }
     }
+    private fun validateFields(){
+        val allFieldsFilled = !editTextUserNameOrEmail!!.text.isNullOrBlank() && !editTextPassword!!.text.isNullOrBlank()
+        ButtonLogin!!.isEnabled = allFieldsFilled
+    }
     fun openRegisterForm(){
-        var intent = Intent(this, RegistrationActivity ::class.java)
+        var intent = Intent(this, RegistrationActivity :: class.java)
         startActivity(intent)
         finish()
     }
--- Software/app/src/main/java/com/example/eventfinder/RegistrationActivity.kt
+++ Software/app/src/main/java/com/example/eventfinder/RegistrationActivity.kt
@@ -2,13 +2,22 @@ package com.example.eventfinder
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+
 class RegistrationActivity : AppCompatActivity() {
     private var button: Button? = null
     private var textViewAlreadyLoggedIn: TextView? = null
+    private var editTextFirstName: EditText? = null
+    private var editTextLastName: EditText? = null
+    private var editTextUsername: EditText? = null
+    private var editTextPassword: EditText? = null
+    private var editTextEmail: EditText? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,15 +25,51 @@ class RegistrationActivity : AppCompatActivity() {
 
         button = findViewById<View>(R.id.buttonRegister) as Button
         textViewAlreadyLoggedIn = findViewById<View>(R.id.textViewAlreadyLoggedIn) as TextView
+        editTextFirstName = findViewById<View>(R.id.editTextFirstName) as EditText
+        editTextLastName = findViewById<View>(R.id.editTextLastName) as EditText
+        editTextUsername = findViewById<View>(R.id.editTextUsername) as EditText
+        editTextPassword = findViewById<View>(R.id.editTextPassword) as EditText
+        editTextEmail = findViewById<View>(R.id.editTextEmail) as EditText
+        button!!.isEnabled = false
+
+        val textWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // Poziva se kad se promijeni tekst u bilo kojem EditText polju
+                validateFields()
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        }
+
+        // Dodajemo TextWatcher na svako EditText polje
+        editTextFirstName!!.addTextChangedListener(textWatcher)
+        editTextLastName!!.addTextChangedListener(textWatcher)
+        editTextUsername!!.addTextChangedListener(textWatcher)
+        editTextPassword!!.addTextChangedListener(textWatcher)
+        editTextEmail!!.addTextChangedListener(textWatcher)
 
         button!!.setOnClickListener { openLogin() }
 
         textViewAlreadyLoggedIn!!.setOnClickListener {
-             openLogin()
+            openLogin()
         }
     }
 
-    fun openLogin() {
+    private fun validateFields() {
+        // Provjeravamo jesu li sva polja ispunjena
+        val allFieldsFilled = !editTextFirstName!!.text.isNullOrBlank() &&
+                !editTextLastName!!.text.isNullOrBlank() &&
+                !editTextUsername!!.text.isNullOrBlank() &&
+                !editTextPassword!!.text.isNullOrBlank() &&
+                !editTextEmail!!.text.isNullOrBlank()
+
+        // Postavljamo stanje gumba ovisno o rezultatu provjere
+        button!!.isEnabled = allFieldsFilled
+    }
+
+    private fun openLogin() {
         val intent = Intent(this, LoginActivity::class.java)
         startActivity(intent)
         finish()
