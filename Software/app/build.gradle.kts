20f952668c76089586059bbd6d7ede888f2f5217
mdesanic21 <mdesanic21@foi.hr>
2023-11-16 17:45:21 +0100

    Implemented polygon drawing, still have to figure out how to place markers

--- Software/app/build.gradle.kts
+++ Software/app/build.gradle.kts
@@ -48,5 +48,6 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("com.google.android.gms:play-services-maps:18.2.0")
+    implementation ("com.google.maps.android:android-maps-utils:2.3.0")
     implementation("com.google.android.gms:play-services-location:21.0.1")
 }
--- Software/app/src/main/java/com/example/eventfinder/MapsFragment.kt
+++ Software/app/src/main/java/com/example/eventfinder/MapsFragment.kt
@@ -1,8 +1,12 @@
 package com.example.eventfinder
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Color
 import android.location.Address
 import android.location.Geocoder
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.FragmentActivity
@@ -11,14 +15,21 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.PolygonOptions
 import java.io.IOException
 
 class MapsFragment : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var myMap: GoogleMap
     private lateinit var binding: FragmentMapsBinding
+    private val polygonPoints = mutableListOf<LatLng>()
+    private var isDrawing = false
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +41,9 @@ class MapsFragment : AppCompatActivity(), OnMapReadyCallback {
             .findFragmentById(R.id.map) as SupportMapFragment
 
         mapFragment.getMapAsync(this)
+
+        val btnPolygon = findViewById<Button>(R.id.btn_polygon)
+
 
         binding.mapSearch.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
@@ -68,9 +82,58 @@ class MapsFragment : AppCompatActivity(), OnMapReadyCallback {
                 return true
             }
         })
+
+        btnPolygon.setOnClickListener {
+            startPolygonDrawing()
+        }
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         myMap = googleMap
+
+        myMap.setOnMapClickListener { point ->
+            if (isDrawing) {
+                addPolygonPoint(point)
+            }
+        }
+    }
+
+    private fun startPolygonDrawing() {
+        isDrawing = true
+        polygonPoints.clear()
+
+        // Clear existing polygons
+        myMap.clear()
+    }
+
+    private fun addPolygonPoint(point: LatLng) {
+        polygonPoints.add(point)
+        val bitmap: Bitmap = BitmapFactory.decodeResource(resources, R.drawable.outline_flag)
+
+        myMap.addMarker(
+            MarkerOptions()
+                .position(point)
+                .title("tocka")
+                .icon(BitmapDescriptorFactory.fromBitmap(bitmap))
+        )
+        // Draw the polygon on the map
+        drawPolygon()
+
+        // You can perform additional actions here, such as updating UI or storing points
+    }
+
+    private fun drawPolygon() {
+        // Clear existing polygons
+        myMap.clear()
+
+        // Draw the polygon with existing points
+        if (polygonPoints.size >= 3) {
+            val polygonOptions = PolygonOptions()
+                .addAll(polygonPoints)
+                .strokeColor(Color.BLACK)
+                .fillColor(Color.WHITE)
+
+            myMap.addPolygon(polygonOptions)
+        }
     }
 }
