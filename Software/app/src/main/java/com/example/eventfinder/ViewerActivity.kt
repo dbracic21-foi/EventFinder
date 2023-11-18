cd6976bde9e8138774ee0dfd4f206553543558d7
Ivan Juras <ijuras21@foi.hr>
2023-11-18 17:18:58 +0100

    Fixed all bugs in mergin event-viewer and master branch. And merged all activites together.

--- Software/app/src/main/java/com/example/eventfinder/ViewerActivity.kt
+++ Software/app/src/main/java/com/example/eventfinder/ViewerActivity.kt
@@ -1,9 +1,11 @@
 package com.example.eventfinder
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -15,10 +17,6 @@ class ViewerActivity: AppCompatActivity() {
     private val mockEvents = MockDataLoader.getDemoData()
     private lateinit var recyclerView: RecyclerView
 
-
-
-
-
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_combobox)
@@ -28,16 +26,21 @@ class ViewerActivity: AppCompatActivity() {
         recyclerView.layoutManager = LinearLayoutManager(this)
 
 
-
-
-
          val items = listOf("Svi", "Zabavni", "Edukativni", "Volonterski" )
 
          val autoComplete : AutoCompleteTextView = findViewById(R.id.auto_complete)
 
          val adapter = ArrayAdapter(this, R.layout.list_item, items)
 
+         val imageView: ImageView = findViewById(R.id.slika)
+
          autoComplete.setAdapter(adapter)
+
+
+        imageView.setOnClickListener {
+            val intent = Intent(this, MapsFragment::class.java)
+            startActivity(intent)
+        }
 
 
 
